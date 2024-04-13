a655b40e006858d6e2c631793a7e215a7864e0a7
David Machuca <davidmachuca.dev@gmail.com>
2024-04-12 22:20:09 -0500

    add: avance

---
+++ src/features/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { MainHeader } from "../components/MainHeader/MainHeader";
+
+export const Home = () => {
+	return (
+		<>
+			<MainHeader />
+			<div>
+				<p>hola</p>
+			</div>
+		</>
+	);
+};
