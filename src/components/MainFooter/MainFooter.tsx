af6c265f0840a5b8c1409e0147fcd0f2c638f1f8
Janeth <djaneth.spr@gmail.com>
2024-05-06 13:38:40 -0500

    add: social al footer

--- src/components/MainFooter/MainFooter.tsx
+++ src/components/MainFooter/MainFooter.tsx
@@ -1,8 +1,25 @@
 import style from "./MainFooter.module.css";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { IoLogoTiktok } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+
 
 export const MainFooter = () => {
 	return (
 		<div className={style.mainFooter__container}>
+			<div className={style.social__container}>
+				<p>Síguenos:</p>
+				<div className={style.social__content}>
+				<FaFacebook fill="#fff" size={25}/>
+				<FaYoutube fill="#fff" size={25}/>
+				<IoLogoTiktok fill="#fff" size={25}/>
+				<FaInstagram fill="#fff" size={25}/>
+				<FaLinkedin fill="#fff" size={25}/>
+				</div>
+			</div>
 			<div className={style.logo__container}>
 				<img
 					src="/public/assets/logo-masterability-v2.png"
